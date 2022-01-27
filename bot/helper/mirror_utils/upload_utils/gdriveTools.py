2a72a204a20ff83c2f445f498427c92d8e729ca4
idsubhasish <95999921+idsubhasish@users.noreply.github.com>
2022-01-27 21:35:19 +0530

    Update gdriveTools.py

--- bot/helper/mirror_utils/upload_utils/gdriveTools.py
+++ bot/helper/mirror_utils/upload_utils/gdriveTools.py
@@ -552,7 +552,7 @@ class GoogleDriveHelper:
                     nxt_page += 1
             telegraph.edit_page(
                 path = self.path[prev_page],
-                title = 'Sᴜʙʜᴀsɪsʜ-Cʟᴏᴜᴅ-Mɪʀʀᴏʀ-&-Lᴇᴇᴄʜ'S Drive Search',
+                title = 'Sᴜʙʜᴀsɪsʜ-Cʟᴏᴜᴅ-Mɪʀʀᴏʀ-&-Lᴇᴇᴄʜ Drive Search',
                 author_name='💓 Subhasish Cloud Mirror & Leech MirrorBot 📺',
                 content=content
             )
